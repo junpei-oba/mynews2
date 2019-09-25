ca3899c9d8bd00b0a46c750ca246562b68bb82b1
EC2 Default User <ec2-user@ip-172-31-29-248.us-east-2.compute.internal>
2019-09-25 07:45:13 +0000

    php-09task-commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 課題4(php-09)
+// 【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
+// web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
+// admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してく
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+// 下記2段は課題4の回答
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+ださい。
