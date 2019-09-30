42ebdbf95b15dd4fef1b828b003b25a807f3d6ac
EC2 Default User <ec2-user@ip-172-31-29-248.us-east-2.compute.internal>
2019-09-30 11:43:29 +0000

    php-11task

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,9 +7,27 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>Mynews</title>
+        <title>MyProfile</title>
     </head>
     <body>
-        <h1>Profile Create</h1>
+<!--課題4(php-11)-->
+                {{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'プロフィール'を埋め込む --}}
+@section('title', 'プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
     </body>
 </html>
