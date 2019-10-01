66a9d46663f32130b23e7b0092f938fd4a442991
EC2 Default User <ec2-user@ip-172-31-29-248.us-east-2.compute.internal>
2019-10-01 14:14:10 +0000

    php-12task

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,17 @@ Route::get('/', function () {
 // admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-// 下記2段は課題4の回答
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+// 下記2段は課題4(php-09)の回答
+// 課題2(php-12)…下記2段に「->middleware('auth')」を追記
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
